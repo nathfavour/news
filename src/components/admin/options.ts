752bcac38c29dad1c4b7570eeb08bf2e59282440
nnathanielleinahtann <nnathanielleinahtann@gmail.com>
2024-07-16 17:25:45 +0000

    testing

--- src/components/admin/options.ts
+++ src/components/admin/options.ts
@@ -2,10 +2,34 @@ import { AdminJSOptions } from 'adminjs';
 
 import componentLoader from './component-loader.js';
 
+
+
+import { UserModel } from '../user/user.model';
+import { CardsModel } from '../cards/cards.model';
+
+
 const options: AdminJSOptions = {
   componentLoader,
   rootPath: '/admin',
-  resources: [],
+  // resources: [],
+
+
+  resources: [
+    {
+      resource: UserModel,
+      options: {
+        /* resource options */
+      },
+    },
+    // other resources...
+    {
+      resource: CardsModel,
+      options: {
+        /* resource options */
+      },
+    },
+  ],
+
   databases: [],
 };
 
